72e46d21f51b9a98ed499ea1e68ccb6ad178629f
Avin-Techv <avin.mathew@techversantinc.com>
2018-12-20 13:49:47 +0530

    editting todo functionality added

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -2,12 +2,11 @@
     <div>
         <input type="text" class="todo-input" placeholder="What needs to be done" v-model="newTodo" @keyup.enter="addTodo">
         <div v-for="(todo, index) in todos" :key="todo.id" class="todo-item">
-        <div>
-            {{ todo.title }}
-        </div>
-        <div class="remove-item" @click="removeTodo(index)">
-            &times;
-        </div>
+            <div class="todo-item-left">
+                <div v-if="!todo.editing" @dblclick="editTodo(todo)" class="todo-item-label"> {{ todo.title }} </div>
+                <input v-else class="todo-item-edit" type="text" v-model="todo.title" @blur="doneEdit(todo)" @keyup.enter="doneEdit(todo)" v-focus @keyup.esc="cancelEdit(todo)"></input>
+            <div class="remove-item" @click="removeTodo(index)"> &times;</div>
+            </div>
         </div>
     </div>
 </template>
@@ -23,15 +22,27 @@
                         'id': 1,
                         'title': 'Finish Vuew Screencast',
                         'completed': false,
+                        'editing': false,
                     },
                     {
                         'id': 2,
                         'title': 'Take over world',
                         'completed': false,
+                        'editing': false,
                     },
                 ]
             }
         },
+
+        directives: {
+        focus: {
+            // directive definition
+            inserted: function (el) {
+            el.focus()
+            }
+        }
+        }, 
+
         methods:{
             addTodo(){
                 if(this.newTodo.trim().length ==0){
@@ -45,6 +56,20 @@
                 })
                 this.newTodo = ""
                 this.idForTodo++
+            },
+
+            editTodo(todo){
+                this.beforeEditCache = to
+                todo.editing = true
+            },
+            
+            doneEdit(todo){
+                todo.editing = false
+            },
+
+            cancelEdit(todo){
+                todo.title = this.beforeEditCache
+                todo.editing = false
             },
 
             removeTodo(index){
@@ -76,7 +101,30 @@
         cursor: pointer;
         margin-left: 14px;
         &:hover{
-            colour: black;
+            color: black;
+        }
+    }
+
+    .todo-item-left{
+        display: flex;
+        align-items: center;
+    }
+
+    .todo-item-label{
+        padding: 10px;
+        border: 1px solid white;
+        margin-left: 12px;
+    }
+
+    .todo-item-edit{
+        font-size: 24px;
+        color: #2c3e50;
+        margin-left: 12px;
+        width: 100%;
+        border: 1px solid #ccc;
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        &:focus{
+            outline: none;
         }
     }
 </style>
