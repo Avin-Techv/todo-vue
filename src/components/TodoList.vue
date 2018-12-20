594213e601327c7ff84a877689237fcf2c1b9acf
Avin-Techv <avin.mathew@techversantinc.com>
2018-12-20 19:33:34 +0530

    add more animations to fade

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,7 +1,8 @@
 <template>
     <div>
         <input type="text" class="todo-input" placeholder="What needs to be done" v-model="newTodo" @keyup.enter="addTodo">
-        <div v-for="(todo, index) in todos" :key="todo.id" class="todo-item">
+        <transition-group name="fade" enter-active-class="animated fadeInUp" leave-active-class="animated fadeOutDown">
+        <div v-for="(todo, index) in todosFiltered" :key="todo.id" class="todo-item" leave-active-class="animated fadeOutDown">
             <div class="todo-item-left">
                 <input type="checkbox" v-model="todo.completed">
                 <div v-if="!todo.editing" @dblclick="editTodo(todo)" class="todo-item-label" :class="{ completed: todo.completed }"> {{ todo.title }} </div>
@@ -9,11 +10,26 @@
             <div class="remove-item" @click="removeTodo(index)"> &times;</div>
             </div>
         </div>
+        </transition-group>
+
 
         <div class="extra-container">
             <div><label><input type="checkbox" :checked="!anyRemaining" @change="checkAllTodos">Check All</label></div>
             <div>{{ remaining }} items left</div>
         </div>
+
+        <div class="extra-container">
+            <div>
+                <button :class="{ active: filter == 'all' }" @click="filter='all'">All</button>
+                <button :class="{ active: filter == 'active' }" @click="filter='active'">Active</button>
+                <button :class="{ active: filter == 'completed' }" @click="filter='completed'">Completed</button>
+            </div>
+
+            <button v-if="showClearCompletedButton" @click="clearCompleted">
+                Clear Completed
+            </button>
+        </div>
+
     </div>
 
 </template>
@@ -26,6 +42,7 @@
                 newTodo: '',
                 idForTodo: 3,
                 beforeEditCache: '',
+                filter: 'all',
                 todos: [{
                         'id': 1,
                         'title': 'Finish Vuew Screencast',
@@ -46,10 +63,22 @@
             remaining(){
                 return this.todos.filter(todo => !todo.completed).length
             },
-
             anyRemaining(){
                 return this.remaining != 0
             },
+            todosFiltered() {
+            if (this.filter == 'all') {
+                return this.todos
+            } else if (this.filter == 'active') {
+                return this.todos.filter(todo => !todo.completed)
+            } else if (this.filter == 'completed') {
+                return this.todos.filter(todo => todo.completed)
+            }
+            return this.todos
+            },
+            showClearCompletedButton(){
+                return this.todos.filter(todo => todo.completed).length > 0 
+            }
         },
 
         directives: {
@@ -97,6 +126,10 @@
                 this.todos.forEach( (todo) => todo.completed = event.target.checked)
             },
 
+            clearCompleted(){
+                this.todos = this.todos.filter(todo => !todo.completed)
+            },
+
             removeTodo(index){
                 this.todos.splice(index, 1)
             }
@@ -105,6 +138,8 @@
 </script>
 
 <style lang="scss">
+    @import url("https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css");
+
     .todo-input {
         width: 100%;
         padding: 10px 18px;
@@ -120,6 +155,7 @@
         display: flex;
         align-items: center;
         justify-content: space-between;
+        animation-duration: 0.3s;
     }
 
     .remove-item{
